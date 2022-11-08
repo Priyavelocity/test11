74ec452cd2f4ec5cd88b8fd4933c0fcaeee06c69
Onkar Naikwadi <Onkar Naikwadi@DESKTOP-P5E6DKT>
2022-11-09 01:32:32 +0530

    First Commit-I

---
+++ src/Start/basic1.java
@@ -0,0 +1,22 @@
+package Start;
+
+public class basic1 
+{
+
+	public static void main(String[]args)
+    {
+    	M1();
+    	System.out.println("it's priya");
+    }
+	
+	public static void M1()
+	{
+		
+		System.out.println("hi");
+	}
+	
+	
+	
+	
+}
+
